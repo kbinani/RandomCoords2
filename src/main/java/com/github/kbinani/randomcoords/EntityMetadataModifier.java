df277f666cc2a147668aa6dd1c0bcc07ccb006e2
kbinani <kbinani.bt@gmail.com>
2021-02-14 18:12:53 +0900

    Cleanup

--- src/main/java/com/github/kbinani/randomcoords/EntityMetadataModifier.java
+++ src/main/java/com/github/kbinani/randomcoords/EntityMetadataModifier.java
@@ -11,34 +11,44 @@ import java.util.Optional;
 
 class EntityMetadataModifier {
     public static void Modify(PacketContainer packet, Point chunkOffset) {
-        try {
-            StructureModifier<List<WrappedWatchableObject>> list = packet.getWatchableCollectionModifier();
-            List<WrappedWatchableObject> objs = list.read(0);
-            for (WrappedWatchableObject obj : objs) {
-                int idx = obj.getIndex();
-                Object v = obj.getRawValue();
-                if (idx == 16) {
-                    // Optional<BlockPosition>
-                    if (v instanceof Optional) {
-                        Optional<?> ov = (Optional<?>) v;
-                        if (ov.isPresent()) {
-                            Object unwrapped = ov.get();
-                            Class<?> c = unwrapped.getClass();
-                            Constructor<?> ctor = c.getDeclaredConstructor(Integer.TYPE, Integer.TYPE, Integer.TYPE);
-                            Method getX = c.getMethod("getX");
-                            int x = (int) getX.invoke(unwrapped);
-                            Method getY = c.getMethod("getY");
-                            int y = (int) getY.invoke(unwrapped);
-                            Method getZ = c.getMethod("getZ");
-                            int z = (int) getZ.invoke(unwrapped);
-                            Object modified = ctor.newInstance(x - (chunkOffset.x << 4), y, z - (chunkOffset.z << 4));
-                            obj.setValue(Optional.of(modified));
-                        }
-                    }
-                }
+        StructureModifier<List<WrappedWatchableObject>> list = packet.getWatchableCollectionModifier();
+        List<WrappedWatchableObject> objs = list.read(0);
+        for (WrappedWatchableObject obj : objs) {
+            int idx = obj.getIndex();
+            Object v = obj.getRawValue();
+            if (idx != 16) {
+                continue;
             }
-        } catch (Exception e) {
-            System.out.println(e.toString());
+            if (!(v instanceof Optional)) {
+                continue;
+            }
+            Optional<?> ov = (Optional<?>) v;
+            if (!ov.isPresent()) {
+                continue;
+            }
+            Object unwrapped = ov.get();
+            Class<?> c = unwrapped.getClass();
+            Constructor<?> ctor;
+            Method getX;
+            Method getY;
+            Method getZ;
+            try {
+                ctor = c.getDeclaredConstructor(Integer.TYPE, Integer.TYPE, Integer.TYPE);
+                getX = c.getMethod("getX");
+                getY = c.getMethod("getY");
+                getZ = c.getMethod("getZ");
+            } catch (Exception e) {
+                continue;
+            }
+            try {
+                int x = (int) getX.invoke(unwrapped);
+                int y = (int) getY.invoke(unwrapped);
+                int z = (int) getZ.invoke(unwrapped);
+                Object modified = ctor.newInstance(x - (chunkOffset.x << 4), y, z - (chunkOffset.z << 4));
+                obj.setValue(Optional.of(modified));
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
         }
     }
 }
